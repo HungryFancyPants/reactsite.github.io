4cc4cff5fc91a15fa1fead36278fe06ad4de686a
HungryFancyPants <101226200+HungryFancyPants@users.noreply.github.com>
2022-03-13 16:50:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(<h6>Ulala Ulala!</h6>, document.getElementById("root2"))
+ReactDOM.render(<ul>List <li>CSS</li><li>JavaScript</li></ul>, document.getElementById("root1"))
